6ca89cdc58c93aa99a6e0e9369ad3772f8a8443c
ihsan606 <ihsansyafiul@gmail.com>
2021-05-24 10:35:36 +0700

    constructor

---
+++ constructor_demo/bin/main.dart
@@ -0,0 +1,11 @@
+import 'Person.dart';
+import 'Student.dart';
+
+void main(List<String> arguments) {
+  // var input = stdin.readLineSync();
+
+  Person p;
+  p = Student();
+  print(p.name);
+  // print('hasil = ' + input);
+}
