0c463dcb0895e06b2f0ef0470ecd8ffda21eac93
ihsan606 <ihsansyafiul@gmail.com>
2021-05-24 10:36:50 +0700

    test

---
+++ constructor_demo/bin/Person.dart
@@ -0,0 +1,8 @@
+class Person {
+  String name;
+
+  Person({String name = 'no name'}) {
+    this.name = name;
+    print('const person');
+  }
+}
---
+++ constructor_demo/bin/Student.dart
@@ -0,0 +1,7 @@
+import 'Person.dart';
+
+class Student extends Person {
+  Student() : super(name: 'Student') {
+    print('const student');
+  }
+}
